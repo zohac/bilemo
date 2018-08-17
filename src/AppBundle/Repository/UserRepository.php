d4823272ac4a711b10eba67cd0efc360b257165a
zohac <fenrir0680@gmail.com>
2018-08-17 10:16:07 +0200

    build(core): creation of listAction method

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\User;
+
 /**
  * UserRepository.
  *
@@ -10,4 +12,21 @@ namespace AppBundle\Repository;
  */
 class UserRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * Return the list of product with all associated entities.
+     *
+     * @param User $user
+     * @return array|null
+     */
+    public function findAllWhithAllEntities(User $user): ?array
+    {
+        return $this->createQueryBuilder('u')
+            ->leftJoin('u.customer', 'c')
+            ->addSelect('c')
+            ->andWhere('c.name = :customer')
+            ->setParameter('customer', $user->getCustomer()->getName())
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
